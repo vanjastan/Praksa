889e7e339b759f1ef38e01ea1464ae062b6e958c
Vanja <vanjastan31@gmail.com>
2021-04-06 11:02:28 +0200

    Added yellow background

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,16 @@
 <template>
-  <v-app>
-    <h1>cao</h1>
+  <v-app style="background-color: #FFF6A1;">
     <v-main>
-      <HelloWorld/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
   },
 
   data: () => ({
