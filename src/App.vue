5d41bbcd287c27b45be568ed27338d2d9f7f7947
Vanja <vanjastan31@gmail.com>
2021-04-06 11:23:51 +0200

    Added 3 blue squares

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <v-app style="background-color: #FFF6A1;">
     <v-main>
+      <Square/>
     </v-main>
   </v-app>
 </template>
 
 <script>
+import Square from './components/Square';
 
 export default {
   name: 'App',
 
   components: {
+    Square,
   },
 
   data: () => ({
---
+++ src/components/Square.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-container>
+   <div class="square1"></div>
+   <div class="square2"></div>
+   <div class="square3"></div>
+  </v-container>
+</template>
+
+<style>
+.square1 {
+  height: 200px;
+  width: 200px;
+  background-color: #0D48E0;
+  margin-top:10%;
+}
+
+.square2 {
+  height: 200px;
+  width: 200px;
+  background-color: #0D48E0;
+  margin-top:10%;
+}
+
+.square3 {
+  height: 200px;
+  width: 200px;
+  background-color: #0D48E0;
+  margin-top:10%;
+}
+</style>
+
+<script>
+  export default {
+    name: 'Square',
+
+    data: () => ({
+
+    }),
+  }
+</script>
