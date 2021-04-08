9218f9a3ce0de27b71f6ae7d57530d381599a1b6
Vanja <vanjastan31@gmail.com>
2021-04-08 09:54:23 +0200

    Red and blue squares

--- src/components/Square.vue
+++ src/components/Square.vue
@@ -4,6 +4,7 @@
    <div 
    v-for="sqr in squares"
    v-bind:style="{'background-color':sqr.color}"
+   v-on:click="changeColor(sqr.id)"
    :key="sqr.id"
    class="square">
    </div>
@@ -36,17 +37,31 @@
                {
                    id: 1,
                    color: "blue",
+                   number: "",
                },
                {
                    id: 2,
                    color: "blue",
+                   number: "",
                },
                {
                    id: 3,
                    color: "blue",
+                   number: "",
                },
            ],
         };
     },
+
+    methods: {
+        changeColor(id) {
+            for (let i = 0; i < this.squares.length; i++) {
+                if(this.squares[i].id === id) {
+                    this.squares[i].color = "red";
+                }
+                else this.squares[i].color = "blue";
+            }
+        }
+    }
   }
 </script>
